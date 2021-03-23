0d43bd71661e1c5bd48004682afd7fa090c69401
marcushaddon <haddon.marcus@gmail.com>
2021-03-23 12:32:11 -0700

    scaffold coinflip endpoint logic

--- demo.js
+++ demo.js
@@ -7,26 +7,37 @@ const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
+  const defaultHeaders = { 'Access-Control-Allow-Origin': '*' };
   if (page == "/") {
     fs.readFile("demofile.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    }); 
-  } else if (page == "/public/css/style.css"){
-     fs.readFile("public/css/style.css", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/css" });
-      res.write(data);
-      res.end();
-     });
-  } else if (page == "/demo.js") {
-    fs.readFile("demo.js", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.writeHead(200, { ...defaultHeaders, "Content-Type": "text/html" });
       res.write(data);
       res.end();
     });
+  } else if (page == "/public/css/style.css") {
+    fs.readFile("public/css/style.css", function (err, data) {
+      res.writeHead(200, { ...defaultHeaders, "Content-Type": "text/css" });
+      res.write(data);
+      res.end();
+    });
+  } else if (page == "/frontend.js") {
+    fs.readFile("public/js/frontend.js", function (err, data) {
+      console.log(data);
+      res.writeHead(200, { ...defaultHeaders, "Content-Type": "text/javascript" });
+      res.write(data);
+      res.end();
+    });
+  } else if (page === '/coinflip') {
+    /** TODO:
+     * actually 'flip' coin and send back result
+     */
+    res.end(JSON.stringify({ result: 'heads' }));
+  } else {
+    res.writeHead(404);
+    res.write(JSON.stringify({ msg: 'NOT FOUND' }));
+    res.end();
   }
 });
 
-server.listen(5004);
+server.listen(5004, () => console.log('Listening on port 5004'));
 
---
+++ public/js/frontend.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('LOADED')
+})
+
+function someFunction() {
+    console.log('called me!');
+    /**
+     * TODO: Make fetch GET request to route for coinflip
+     * handle result (should be a string that can be 
+     * JSON.parse'd), and update the dom appropriately
+     * to show/hide the correct quarter
+     */
+}
